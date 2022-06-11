1b4c076fd1076b84823649806338376c58d06cf5
Yannick Sereckissy <yannick.sereckissy@gmail.com>
2022-06-11 15:51:36 +0200

    fix some performance issues web.dev

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["images.unsplash.com"]
   },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Important: return the modified config
+    return config
+  },
 }
